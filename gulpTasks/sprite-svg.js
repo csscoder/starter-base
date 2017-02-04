f8b79afcd217f16b53b44d832b20d04ccd94e192
Alexander Zidyganov <alex@csscoder.pro>
2017-02-04 08:29:04 +0500

    update svg-sprite

--- gulpTasks/sprite-svg.js
+++ gulpTasks/sprite-svg.js
@@ -5,6 +5,7 @@ import gulp from 'gulp';
 import svgSymbols from 'gulp-svg-symbols';
 import path from 'path';
 import filter from 'gulp-filter';
+import babel from 'gulp-babel';
 
 gulp.task('svg-sprites', function () {
 
@@ -22,6 +23,9 @@ gulp.task('svg-sprites', function () {
       ]
     }))
     .pipe(jsFilter)
+    .pipe(babel({
+      presets: ['es2015']
+    }))
     .pipe(gulp.dest(config.js.dest))
     .pipe(jsFilter.restore)
     .pipe(scssFilter)
